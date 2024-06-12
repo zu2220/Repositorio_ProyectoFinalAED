b1ec6978ad8a0788231da123dd49f1f4bf6db7fd
dootpolar <dootpolar@gmail.com>
2024-06-11 23:25:31 -0500

    frmEliminar ext .java

--- ProyectoAED/src/Formularios/frmEliminar.java
+++ ProyectoAED/src/Formularios/frmEliminar.java
@@ -30,7 +30,7 @@ public class frmEliminar extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("ELIMINAR");
+        jLabel1.setText("eliminarActualizado");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -39,7 +39,7 @@ public class frmEliminar extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(170, 170, 170)
                 .addComponent(jLabel1)
-                .addContainerGap(177, Short.MAX_VALUE))
+                .addContainerGap(124, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
