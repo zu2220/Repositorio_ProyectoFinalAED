5f48e012418c5f8994d79f6816b098afeb71084b
HectorSiancas <U22312324@utp.edu.pe>
2024-07-09 10:31:03 -0500

    Proyecto configurado: Grafos AED Final

---
+++ JavaApplication14/src/ProyectoFinal/Nodo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ProyectoFinal;
+
+/**
+ *
+ * @author Hector
+ */
+public class Nodo {
+    //No le cambiaras el nombre a Nodo Estudiante
+    //Ya que evitaras confusiones
+    
+    
+}
